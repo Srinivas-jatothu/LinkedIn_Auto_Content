20094ec8c7a1bc266125f5f841b5c47d2f5dc6b0
Srinivas-jatothu <cs21b023@iittp.ac.in>
2025-01-14 18:48:22 +0530

    First commit

---
+++ testing.py
@@ -0,0 +1,4 @@
+#this project is to create a post generator for linkedin
+#this is the testing file for the project
+#we will us the print stmt for testing the git commands
+print("testing the git commands")
