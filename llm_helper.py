f411c7c82c2631f3f8902a867354936c378ef7fb
Srinivas-jatothu <cs21b023@iittp.ac.in>
2025-01-15 15:33:59 +0530

    Preproecessing the text data in json with llm

--- llm_helper.py
+++ llm_helper.py
@@ -3,12 +3,7 @@ import os
 # from langchain.llms import ChatGroq
 from langchain_groq import ChatGroq
 
-
-
-
-
 load_dotenv()
-
 llm=ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"),model_name="llama-3.3-70b-specdec")
 
 if __name__ == "__main__":
